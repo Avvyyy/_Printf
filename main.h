1125ae553e2c6d6c747a4fdc44da1bbe1332d04c
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-23 22:55:13 -0700

    Corrections to main.h

--- main.h
+++ main.h
@@ -35,6 +35,7 @@ typedef struct printHandler
 
 int print_int(va_list l, flags_t *f);
 void print_number(int n);
+int _printint(int n);
 int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
 int print_hex(va_list l, flags_t *f);
